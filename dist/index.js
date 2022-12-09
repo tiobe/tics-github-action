43ac01d3689aa44e9319e2f4e374deb302da1616
Luuk Janssen <118828444+luuk-janssen-tiobe@users.noreply.github.com>
2022-12-09 15:27:18 +0100

    layout mistakes

--- dist/index.js
+++ dist/index.js
@@ -465,16 +465,17 @@ function createUnpostableReviewCommentsSummary(unpostableReviewComments) {
     let previousPath = '';
     unpostableReviewComments.forEach(reviewComment => {
         if (previousPath === '') {
-            body += `<table><tr><td rowspan="2">:warning:</td><th>${reviewComment.path}</th></tr>`;
+            body += `<table><tr><th colspan='2'>${reviewComment.path}</th></tr>`;
         }
-        else if (previousPath !== reviewComment.path) {
-            body += `</table><table><tr><td rowspan="2">:warning:</td><th>${reviewComment.path}</th></tr>`;
+        if (previousPath !== reviewComment.path) {
+            body += `<table><tr><th colspan='2'>${reviewComment.path}</th></tr>`;
         }
         else {
-            body += `<tr><td><b>TiCS: ${reviewComment.type} violation: ${reviewComment.msg}</b><br>${reviewComment.displayCount}Line: ${reviewComment.line}, Rule: ${reviewComment.rule}, Level: ${reviewComment.level}, Category: ${reviewComment.category}</td></tr>`;
+            body += `<tr><td>:warning:</td><td><b>TiCS: ${reviewComment.type} violation: ${reviewComment.msg}</b><br>${reviewComment.displayCount}Line: ${reviewComment.line}, Rule: ${reviewComment.rule}, Level: ${reviewComment.level}, Category: ${reviewComment.category}</td></tr>`;
         }
         previousPath = reviewComment.path;
     });
+    body += '</table>';
     return (0, markdown_1.generateExpandableAreaMarkdown)(header, body);
 }
 exports.createUnpostableReviewCommentsSummary = createUnpostableReviewCommentsSummary;
