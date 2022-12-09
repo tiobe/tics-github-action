4981b327ff2fa63c5ffa1ae21505492e45087af4
Luuk Janssen <118828444+luuk-janssen-tiobe@users.noreply.github.com>
2022-12-09 14:12:59 +0100

    now using includes to clean it up

--- src/github/posting/summary.ts
+++ src/github/posting/summary.ts
@@ -122,7 +122,7 @@ export async function createReviewComments(annotations: any[], changedFiles: any
   groupedAnnotations.forEach(annotation => {
     Logger.Instance.debug(JSON.stringify(annotation));
     const displayCount = annotation.count === 1 ? '' : `(${annotation.count}x) `;
-    if (annotation.diffLines.filter((d: number) => d === 1).length >= 0) {
+    if (annotation.diffLines.includes(annotation.line)) {
       postable.push({
         body: `:warning: **TiCS: ${annotation.type} violation: ${annotation.msg}** \r\n${displayCount}Line: ${annotation.line}, Rule: ${annotation.rule}, Level: ${annotation.level}, Category: ${annotation.category} \r\n`,
         path: annotation.fullPath.replace(`HIE://${ticsConfig.projectName}/${ticsConfig.branchName}/`, ''),
