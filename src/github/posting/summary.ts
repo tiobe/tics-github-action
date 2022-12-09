283d1ba55290cfb5bcb5096f0a962983b0c6ccfc
Luuk Janssen <118828444+luuk-janssen-tiobe@users.noreply.github.com>
2022-12-09 13:56:59 +0100

    testing unpostable

--- src/github/posting/summary.ts
+++ src/github/posting/summary.ts
@@ -122,7 +122,7 @@ export async function createReviewComments(annotations: any[], changedFiles: any
   groupedAnnotations.forEach(annotation => {
     Logger.Instance.debug(JSON.stringify(annotation));
     const displayCount = annotation.count === 1 ? '' : `(${annotation.count}x) `;
-    if (annotation.diffLines.find((d: number) => d === annotation.line)) {
+    if (annotation.diffLines.find((d: number) => d === 1)) {
       postable.push({
         body: `:warning: **TiCS: ${annotation.type} violation: ${annotation.msg}** \r\n${displayCount}Line: ${annotation.line}, Rule: ${annotation.rule}, Level: ${annotation.level}, Category: ${annotation.category} \r\n`,
         path: annotation.fullPath.replace(`HIE://${ticsConfig.projectName}/${ticsConfig.branchName}/`, ''),
