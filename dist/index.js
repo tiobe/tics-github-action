cdc6dadd8452558a84bb736703a00101eb2f1acb
TIOBE <123092508+it-tiobe@users.noreply.github.com>
2023-07-11 09:53:36 +0000

    Build dist after push to main

--- dist/index.js
+++ dist/index.js
@@ -22526,6 +22526,43 @@ module.exports.canonical = canonical;
     0;
 })(this, (function (exports) { 'use strict';
 
+    const semver = /^[v^~<>=]*?(\d+)(?:\.([x*]|\d+)(?:\.([x*]|\d+)(?:\.([x*]|\d+))?(?:-([\da-z\-]+(?:\.[\da-z\-]+)*))?(?:\+[\da-z\-]+(?:\.[\da-z\-]+)*)?)?)?$/i;
+    const validateAndParse = (version) => {
+        if (typeof version !== 'string') {
+            throw new TypeError('Invalid argument expected string');
+        }
+        const match = version.match(semver);
+        if (!match) {
+            throw new Error(`Invalid argument not valid semver ('${version}' received)`);
+        }
+        match.shift();
+        return match;
+    };
+    const isWildcard = (s) => s === '*' || s === 'x' || s === 'X';
+    const tryParse = (v) => {
+        const n = parseInt(v, 10);
+        return isNaN(n) ? v : n;
+    };
+    const forceType = (a, b) => typeof a !== typeof b ? [String(a), String(b)] : [a, b];
+    const compareStrings = (a, b) => {
+        if (isWildcard(a) || isWildcard(b))
+            return 0;
+        const [ap, bp] = forceType(tryParse(a), tryParse(b));
+        if (ap > bp)
+            return 1;
+        if (ap < bp)
+            return -1;
+        return 0;
+    };
+    const compareSegments = (a, b) => {
+        for (let i = 0; i < Math.max(a.length, b.length); i++) {
+            const r = compareStrings(a[i] || '0', b[i] || '0');
+            if (r !== 0)
+                return r;
+        }
+        return 0;
+    };
+
     /**
      * Compare [semver](https://semver.org/) version strings to find greater, equal or lesser.
      * This library supports the full semver specification, including comparing versions with different number of digits like `1.0.0`, `1.0`, `1`, and pre-release versions like `1.0.0-alpha`.
@@ -22553,20 +22590,7 @@ module.exports.canonical = canonical;
         }
         return 0;
     };
-    /**
-     * Validate [semver](https://semver.org/) version strings.
-     *
-     * @param version Version number to validate
-     * @returns `true` if the version number is a valid semver version number, `false` otherwise.
-     *
-     * @example
-     * ```
-     * validate('1.0.0-rc.1'); // return true
-     * validate('1.0-rc.1'); // return false
-     * validate('foo'); // return false
-     * ```
-     */
-    const validate = (version) => typeof version === 'string' && /^[v\d]/.test(version) && semver.test(version);
+
     /**
      * Compare [semver](https://semver.org/) version strings using the specified operator.
      *
@@ -22592,6 +22616,24 @@ module.exports.canonical = canonical;
         const res = compareVersions(v1, v2);
         return operatorResMap[operator].includes(res);
     };
+    const operatorResMap = {
+        '>': [1],
+        '>=': [0, 1],
+        '=': [0],
+        '<=': [-1, 0],
+        '<': [-1],
+        '!=': [-1, 1],
+    };
+    const allowedOperators = Object.keys(operatorResMap);
+    const assertValidOperator = (op) => {
+        if (typeof op !== 'string') {
+            throw new TypeError(`Invalid operator type, expected string but got ${typeof op}`);
+        }
+        if (allowedOperators.indexOf(op) === -1) {
+            throw new Error(`Invalid operator, expected one of ${allowedOperators.join('|')}`);
+        }
+    };
+
     /**
      * Match [npm semver](https://docs.npmjs.com/cli/v6/using-npm/semver) version range.
      *
@@ -22606,9 +22648,15 @@ module.exports.canonical = canonical;
      * ```
      */
     const satisfies = (version, range) => {
+        // clean input
+        range = range.replace(/([><=]+)\s+/g, '$1');
         // handle multiple comparators
         if (range.includes('||')) {
             return range.split('||').some((r) => satisfies(version, r));
+        }
+        else if (range.includes(' - ')) {
+            const [a, b] = range.split(' - ', 2);
+            return satisfies(version, `>=${a} <=${b}`);
         }
         else if (range.includes(' ')) {
             return range
@@ -22649,58 +22697,21 @@ module.exports.canonical = canonical;
             return false;
         return true;
     };
-    const semver = /^[v^~<>=]*?(\d+)(?:\.([x*]|\d+)(?:\.([x*]|\d+)(?:\.([x*]|\d+))?(?:-([\da-z\-]+(?:\.[\da-z\-]+)*))?(?:\+[\da-z\-]+(?:\.[\da-z\-]+)*)?)?)?$/i;
-    const validateAndParse = (version) => {
-        if (typeof version !== 'string') {
-            throw new TypeError('Invalid argument expected string');
-        }
-        const match = version.match(semver);
-        if (!match) {
-            throw new Error(`Invalid argument not valid semver ('${version}' received)`);
-        }
-        match.shift();
-        return match;
-    };
-    const isWildcard = (s) => s === '*' || s === 'x' || s === 'X';
-    const tryParse = (v) => {
-        const n = parseInt(v, 10);
-        return isNaN(n) ? v : n;
-    };
-    const forceType = (a, b) => typeof a !== typeof b ? [String(a), String(b)] : [a, b];
-    const compareStrings = (a, b) => {
-        if (isWildcard(a) || isWildcard(b))
-            return 0;
-        const [ap, bp] = forceType(tryParse(a), tryParse(b));
-        if (ap > bp)
-            return 1;
-        if (ap < bp)
-            return -1;
-        return 0;
-    };
-    const compareSegments = (a, b) => {
-        for (let i = 0; i < Math.max(a.length, b.length); i++) {
-            const r = compareStrings(a[i] || '0', b[i] || '0');
-            if (r !== 0)
-                return r;
-        }
-        return 0;
-    };
-    const operatorResMap = {
-        '>': [1],
-        '>=': [0, 1],
-        '=': [0],
-        '<=': [-1, 0],
-        '<': [-1],
-    };
-    const allowedOperators = Object.keys(operatorResMap);
-    const assertValidOperator = (op) => {
-        if (typeof op !== 'string') {
-            throw new TypeError(`Invalid operator type, expected string but got ${typeof op}`);
-        }
-        if (allowedOperators.indexOf(op) === -1) {
-            throw new Error(`Invalid operator, expected one of ${allowedOperators.join('|')}`);
-        }
-    };
+
+    /**
+     * Validate [semver](https://semver.org/) version strings.
+     *
+     * @param version Version number to validate
+     * @returns `true` if the version number is a valid semver version number, `false` otherwise.
+     *
+     * @example
+     * ```
+     * validate('1.0.0-rc.1'); // return true
+     * validate('1.0-rc.1'); // return false
+     * validate('foo'); // return false
+     * ```
+     */
+    const validate = (version) => typeof version === 'string' && /^[v\d]/.test(version) && semver.test(version);
 
     exports.compare = compare;
     exports.compareVersions = compareVersions;
